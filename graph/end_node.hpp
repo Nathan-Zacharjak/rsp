e835b2846e96dce9c199d7582e5d5238d4df2593
Nathan-Zacharjak <nzac066@outlook.com>
2024-11-28 16:24:41 +1030

    Added start of Graph adjacency list implementation

---
+++ graph/end_node.hpp
@@ -0,0 +1,28 @@
+#ifndef _END_NODE_HPP_
+#define _END_NODE_HPP_
+
+#include <string>
+
+using namespace std;
+
+class EndNode
+{
+private:
+public:
+    string label;
+    int weight;
+    EndNode(string label, int weight);
+    ~EndNode();
+};
+
+EndNode::EndNode(string label, int weight)
+{
+    this->label = label;
+    this->weight = 0;
+}
+
+EndNode::~EndNode()
+{
+}
+
+#endif
---
+++ graph/graph.hpp
@@ -0,0 +1,59 @@
+#ifndef _GRAPH_HPP_
+#define _GRAPH_HPP_
+
+#include "end_node.hpp"
+#include <iostream>
+#include <string>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+using namespace std;
+
+class Graph
+{
+private:
+    unordered_map<string, unordered_set<EndNode *>> adjacencyList;
+
+public:
+    Graph();
+
+    void InsertNode(string node);
+    void InsertEdge(string startNode, string endNode, int weight);
+
+    void PrintEdges(string node);
+    void PrintAllEdges(void);
+
+    EndNode *FindNodeDFS(string node);
+    EndNode *FindNodeBFS(string node);
+
+    void DeleteEdge(string startNode, string endNode);
+    void DeleteNode(string deleteNode);
+
+    ~Graph();
+};
+
+Graph::Graph()
+{
+}
+
+void Graph::InsertNode(string node)
+{
+    adjacencyList.emplace(node);
+}
+
+void Graph::PrintEdges(string node)
+{
+    unordered_set<EndNode *> edges = this->adjacencyList.at(node);
+
+    for (const auto &edge : edges)
+    {
+        cout << node << " -> " << edge->label << " Weight: " << edge->weight << endl;
+    }
+}
+
+Graph::~Graph()
+{
+}
+
+#endif
