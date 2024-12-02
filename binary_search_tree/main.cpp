b74dbbe588501980f7f942cebe161c4fa35d89c7
Nathan-Zacharjak <nzac066@outlook.com>
2024-12-02 15:52:48 +1030

    Implemented SearchTree()

--- binary_search_tree/main.cpp
+++ binary_search_tree/main.cpp
@@ -11,12 +11,14 @@ int main(int argc, char const *argv[])
     tree.InsertNode(3);
     tree.InsertNode(1);
     tree.InsertNode(2);
-    // tree.InsertNode(1);
-    // tree.InsertNode(4);
-    // tree.InsertNode(5);
-    // tree.InsertNode(6);
+    tree.InsertNode(1);
+    tree.InsertNode(4);
+    tree.InsertNode(5);
+    tree.InsertNode(6);
 
     tree.PrintTree();
+
+    tree.SearchTree(0);
 
     return 0;
 }
--- binary_search_tree/tree.cpp
+++ binary_search_tree/tree.cpp
@@ -57,3 +57,50 @@ void Tree::PrintTree(void)
 {
     this->PrintTreeHelper(this->root);
 }
+
+TreeNodePtr Tree::SearchTree(int data)
+{
+    TreeNodePtr currentNode = this->root;
+
+    if (currentNode == nullptr)
+    {
+        cout << "Tried to search an empty tree!" << endl;
+        return nullptr;
+    }
+
+    while (currentNode != nullptr)
+    {
+        if (currentNode->data == data)
+        {
+            cout << "Found node with data: " << currentNode->data << endl;
+
+            if (currentNode != this->root)
+            {
+                cout << " and parent: " << currentNode->parent->data << endl;
+            }
+
+            return currentNode;
+        }
+        else if (data < currentNode->data)
+        {
+            if (currentNode->left != nullptr)
+            {
+                cout << "Traversing tree left from " << currentNode->data << " to " << currentNode->left->data << endl;
+            }
+
+            currentNode = currentNode->left;
+        }
+        else
+        {
+            if (currentNode->right != nullptr)
+            {
+                cout << "Traversing tree right from " << currentNode->data << " to " << currentNode->right->data << endl;
+            }
+
+            currentNode = currentNode->right;
+        }
+    }
+
+    cout << "Couldn't find node with data: " << data << endl;
+    return nullptr;
+}
