c6cf8cf2df8fe33372930ec8213b1c8c66150017
Nathan-Zacharjak <nzac066@outlook.com>
2024-12-04 16:30:51 +1030

    Added C++ sandbox

---
+++ sandbox/sandbox.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+using std::cout, std::endl, std::string;
+
+int main(int argc, char const *argv[])
+{
+    string hello = "hello!";
+
+    cout << hello << "\n";
+
+    return 0;
+}
