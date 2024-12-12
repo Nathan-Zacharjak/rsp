541b881e37f4517fae35207bd7cbc10b4c4b75a9
Nathan-Zacharjak <nzac066@outlook.com>
2024-12-12 14:06:56 +1030

    Finished array_list

--- array_list/array_list.cpp
+++ array_list/array_list.cpp
@@ -1,12 +1,16 @@
 #include <array>
 #include <bits/unique_ptr.h>
 #include "array_list.h"
+#include <iostream>
+
+using std::cout;
 
 // template <typename T>
 void array_list::insert(int newValue)
 {
     if (_currentSize >= _capacity)
     {
+        cout << "Capacity hit! " << _currentSize << " " << _capacity << "\n";
         _capacity *= _resizeFactor;
         // std::unique_ptr<std::array<int, _size>> newList{};
         // std::unique_ptr<std::array<int, _size>> newList = std::make_unique<std::array<int, _size>>;
@@ -19,6 +23,7 @@ void array_list::insert(int newValue)
 
         delete[] _list;
         _list = newList;
+        cout << "New size and capacity: " << _currentSize << " " << _capacity << "\n";
     }
 
     _list[_currentSize] = newValue;
--- array_list/main.cpp
+++ array_list/main.cpp
@@ -5,11 +5,21 @@
 int main(int argc, char const *argv[])
 {
     array_list list;
-    list.insert(1);
-    list.at(0) = 9;
-    std::cout << list.at(0) << std::endl;
+    for (int i = 0; i < 9; i++)
+    {
+        list.insert(i);
+    }
 
-    std::println("Second arg: {1}, First arg: {0}", list.at(0), 2);
+    // list.insert(1);
+    list.at(0) = 9;
+    // std::cout << list.at(0) << std::endl;
+
+    for (int i = 0; i < 9; i++)
+    {
+        std::println("Index: {} = {}", i, list.at(i));
+    }
+
+    // std::println("Second arg: {1}, First arg: {0}", list.at(0), 2);
 
     return 0;
 }
